ed4c03c144a3b74b5310a13ea3c7378d68628e32
Pavankumar-1000 <126801594+Pavankumar-1000@users.noreply.github.com>
2023-08-29 16:14:32 +0530

    c1

---
+++ pro1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class pro1 {
+public static void main(String args[])
+{
+    Scanner scan=new Scanner(System.in);
+    System.out.print("enter the value of A:");
+    int a=scan.nextInt();
+    System.out.print("\n entr the value of B:");
+    int b=scan.nextInt();
+   int c=a+b;
+    System.out.println("\n the sum of a and b:"+c);
+}
+}
